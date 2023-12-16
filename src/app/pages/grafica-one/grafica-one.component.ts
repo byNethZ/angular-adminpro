8cdf095a489a3a3f7762bb94651a34f042c87586
Nethz Jimenez <bynethz@gmail.com>
2023-12-16 19:59:29 +0000

    Seccion 3 Diseño Terminado

---
+++ src/app/pages/grafica-one/grafica-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica-one',
+  templateUrl: './grafica-one.component.html',
+  styles: ``
+})
+export class GraficaOneComponent {
+
+}
